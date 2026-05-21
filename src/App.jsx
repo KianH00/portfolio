83a83d58effe2289269590bb60d79a237067636e
Kian Hellie <kianhellie00@gmail.com>
2026-05-21 18:30:23 +0800

    fixed case sensitive

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
 import About from "./components/about";
 import Certificates from "./components/certificates";
